70fbb0358f6c1f04103f2573aa017a3ddd63ac17
Kondaveeti Satyanarayana <ksatyanarayana@altimetrik.com>
2022-03-08 10:44:31 +0530

    port changed

--- ScheduleService/Program.cs
+++ ScheduleService/Program.cs
@@ -28,7 +28,7 @@ namespace ScheduleService
        public static void Main(string[] args)
         {
             var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
-            var url = config["ASPNETCORE_URLS"] ?? "http://*:5000";
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:80";
 
             var host = new WebHostBuilder()
                 .UseKestrel()
