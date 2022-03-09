188842af26148d91529b12626e8e82198b549d6b
Tarun Aggarwal <taggarwal@altimetrik.com>
2022-03-09 17:25:45 +0530

    Serilog

--- ScheduleService/Controllers/WeatherForecastController.cs
+++ ScheduleService/Controllers/WeatherForecastController.cs
@@ -16,17 +16,19 @@ namespace ScheduleService.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
         
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            this.logger.LogInformation("Operation Aurguments");
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
--- ScheduleService/Program.cs
+++ ScheduleService/Program.cs
@@ -1,7 +1,10 @@
+using Destructurama;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,9 +15,33 @@ namespace ScheduleService
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+         .SetBasePath(Directory.GetCurrentDirectory())
+         .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+         .AddEnvironmentVariables()
+         .Build();
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            string appVersion = "1.0.0";
+
+            Log.Logger = new LoggerConfiguration().Destructure.UsingAttributes().ReadFrom.Configuration(Configuration)
+                            .Enrich.WithProperty("Version", appVersion)
+                            .WriteTo.File(new JsonFormatter(), "log.txt", rollingInterval: RollingInterval.Day)
+                            .CreateLogger();
+            try
+            {
+                Log.Debug("Starting Payment Safe web host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,6 +49,10 @@ namespace ScheduleService
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                }).ConfigureLogging((context, logging) =>
+                {
+                    logging.ClearProviders();
+                    logging.AddSerilog();
                 });
     }
 }
